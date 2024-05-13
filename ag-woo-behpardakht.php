4b1c5baad813d6ef6bd65c913cac57d30457aa5b
Amirhossein Garousi <59143499+garousiamir@users.noreply.github.com>
2024-05-13 15:59:15 +0330

    fixed for repo

--- ag-woo-behpardakht.php
+++ ag-woo-behpardakht.php
@@ -12,7 +12,7 @@ Requires at least: 6.0
 Tested up to: 6.5.2
 Requires PHP: 7.4
 Stable tag: 1.0
-Text Domain: ag-jalali-date
+Text Domain: ag-woo-behpardakht
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
